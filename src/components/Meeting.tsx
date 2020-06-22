0b876f8ac7c238a4c16a0349e1fb822fe47f376a
Anton Forbes <anton.forbes@airelogic.com>
2020-06-22 16:39:13 +0100

    start session

--- src/components/Meeting.tsx
+++ src/components/Meeting.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from 'react'
-import { ConsoleLogger, DefaultDeviceController, DefaultMeetingSession, LogLevel, MeetingSessionConfiguration } from 'amazon-chime-sdk-js';
+import { ConsoleLogger, DefaultDeviceController, DefaultMeetingSession, LogLevel, MeetingSessionConfiguration, MeetingSessionStatusCode } from 'amazon-chime-sdk-js';
   
 interface IMeetingInfo {
     started: boolean,
@@ -14,10 +14,11 @@ export const Meeting = () => {
     const [ meetingSession, setMeetingSession ] = useState<any|null>(null);
 
     const videoRef = useRef<HTMLVideoElement>(null!);
+    const videoRef2 = useRef<HTMLVideoElement>(null!);
     const audioRef = useRef<HTMLAudioElement>(null!);
 
     const startMeeting = async () => {
-        fetch('https://rrz7e3wd7l.execute-api.eu-west-2.amazonaws.com/dev/room', {method: 'POST'})
+        fetch('https://mbsnz79c79.execute-api.eu-west-2.amazonaws.com/dev/room', {method: 'POST'})
         .then(resp => resp.json())
         .then(json =>
             setMeetingInfo({ started: true, meeting: json.meeting, attendee: json.attendee })
@@ -31,6 +32,62 @@ export const Meeting = () => {
 
         meetingSession.audioVideo.startVideoPreviewForVideoInput(videoRef.current);
     }
+
+
+
+    const observer = {
+        audioVideoDidStart: () => {
+          console.log('Started');
+        },
+
+        videoTileDidUpdate: (tileState: { boundAttendeeId: any; localTile: any; isContent: any; tileId: any; }) => {
+            // Ignore a tile without attendee ID, a local tile (your video), and a content share.
+
+            if (!tileState.boundAttendeeId || tileState.isContent) {
+              return;
+            }
+
+            if(tileState.localTile)
+            {
+                meetingSession.audioVideo.bindVideoElement(tileState.tileId, videoRef.current);
+            }
+            else
+            {        
+            meetingSession.audioVideo.bindVideoElement(tileState.tileId, videoRef2.current);
+            }
+          },
+          audioVideoDidStop: (sessionStatus: { statusCode: () => any; }) => {
+            const sessionStatusCode = sessionStatus.statusCode();
+            if (sessionStatusCode === MeetingSessionStatusCode.Left) {
+              /*
+                - You called meetingSession.audioVideo.stop().
+                - When closing a browser window or page, Chime SDK attempts to leave the session.
+              */
+              console.log('You left the session');
+            } else {
+              console.log('Stopped with a session status code: ', sessionStatusCode);
+            }
+          }
+      };
+
+    const startSession = async () =>{
+        meetingSession.audioVideo.addObserver(observer);
+        meetingSession.audioVideo.start();
+    }
+
+    const stopSession = async() => {
+        meetingSession.audioVideo.stop();
+    }
+
+    const toggleMyVideo = async () => {
+        const videoInputDevices = await meetingSession.audioVideo.listVideoInputDevices();
+        const videoInputDeviceInfo = videoInputDevices[0];
+        await meetingSession.audioVideo.chooseVideoInputDevice(videoInputDeviceInfo.deviceId);
+
+        meetingSession.audioVideo.startLocalVideoTile();
+    }
+
+
 
     useEffect(() => {
 
@@ -51,10 +108,10 @@ export const Meeting = () => {
 
     }, [meetingInfo] )
 
-    useEffect(() => {
-        if(meetingSession != null)
-            startPreview();
-    }, [meetingSession])
+    // useEffect(() => {
+    //     if(meetingSession != null)
+    //         //startPreview();
+    // }, [meetingSession])
 
     const connectDevices = async () => {
         const audioInputDevices = await meetingSession.audioVideo.listAudioInputDevices();
@@ -94,7 +151,11 @@ export const Meeting = () => {
         <>
         <audio ref={audioRef} />
         <video style={{border: "solid 1px black", display: "block"}} ref={videoRef} />
+        <video style={{border: "solid 1px black", display: "block"}} ref={videoRef2} />
         <button onClick={startMeeting}>Start</button>
+        <button onClick={startSession}>Start Session</button>
+        <button onClick={stopSession}>Stop Session</button>
+        <button onClick={toggleMyVideo}>Toggle my video</button>
         </>
     )
 }
